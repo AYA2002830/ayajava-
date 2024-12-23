a70b5c8b14a517bfb0ed8d43180e8a04d9e96612
AYA2002830 <22020190@students.hebron.edu>
2024-12-23 22:07:23 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
-app.get("/name", (req, res) => {
-  res.send("Aya"); 
+app.get("/", (req, res) => {
+    res.send("Aya"); 
 });
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my application!');
-}); 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
